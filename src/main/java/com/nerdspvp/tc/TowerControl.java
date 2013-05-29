3bbe04f69d617f5177b47bb259c64fd0c265e979
NerdsWBNerds <richcoll97@gmail.com>
2013-05-29 16:16:00 -0500

    We made changes; removed unused import.

--- src/main/java/com/nerdspvp/tc/TowerControl.java
+++ src/main/java/com/nerdspvp/tc/TowerControl.java
@@ -1,12 +1,10 @@
 package com.nerdspvp.tc;
 
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TowerControl extends JavaPlugin {
     public void onDisable() {
+        //we made a change
     }
 
     public void onEnable() {
