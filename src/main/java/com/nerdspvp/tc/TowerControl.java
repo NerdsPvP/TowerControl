88885e23a1f5d17b0ba00afcf086d5462ec3b5a5
brenhein@brenhein.org <brenhein@brenhein.org>
2013-05-29 17:18:39 -0400

    Changes :)

--- src/main/java/com/nerdspvp/tc/TowerControl.java
+++ src/main/java/com/nerdspvp/tc/TowerControl.java
@@ -4,7 +4,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class TowerControl extends JavaPlugin {
     public void onDisable() {
-        //we made a change
+
+        // lol changes
     }
 
     public void onEnable() {
